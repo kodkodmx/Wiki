521c805980f66bfccebc845d3810801c30e66adf
Alberto Haboba <betoh@hotmail.com>
2023-04-26 20:22:03 -0600

    Entry Page lista

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
@@ -8,3 +9,18 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def content(request, title):
+    if title == "css" or title == "html":
+        title = title.upper()
+    else:
+        title = title.capitalize() 
+    if title not in util.list_entries():
+        return render(request, "encyclopedia/error.html", {
+            "title": title , "message": "The requested page was not found."
+        })
+    else:
+        markdowner = Markdown()
+        content = markdowner.convert(util.get_entry(title))
+        return render(request, "encyclopedia/content.html", {
+            "content": content , "title": title
+    })
