aaabc9131c89ea0e29bb242539a807aba945aec1
Alberto Haboba <betoh@hotmail.com>
2023-04-27 23:32:20 -0600

    search ready

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.content, name="title")
+    path("wiki/<str:title>", views.content, name="title"),
+    path("search", views.search, name="search"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -42,3 +42,58 @@ def content(request, title):
             "title": title , "message": "The requested page was not found."
         })
     
+def search(request):
+    if request.method == "POST":
+        title = request.POST.get('q')
+        if title in util.list_entries():
+            markdowner = Markdown()
+            content = markdowner.convert(util.get_entry(title))
+            return render(request, "encyclopedia/content.html", {
+                "content": content , "title": title
+            })
+        elif title.lower() in util.list_entries():
+            title=title.lower()
+            markdowner = Markdown()
+            content = markdowner.convert(util.get_entry(title))
+            return render(request, "encyclopedia/content.html", {
+                "content": content , "title": title
+            })
+        elif title.upper() in util.list_entries():
+            title=title.upper()
+            markdowner = Markdown()
+            content = markdowner.convert(util.get_entry(title))
+            return render(request, "encyclopedia/content.html", {
+                "content": content , "title": title
+            })
+        elif title.capitalize() in util.list_entries():
+            title=title.capitalize()
+            markdowner = Markdown()
+            content = markdowner.convert(util.get_entry(title))
+            return render(request, "encyclopedia/content.html", {
+                "content": content , "title": title
+            })
+        else:
+            title = request.POST.get('q')
+            entries = []
+            for entry in util.list_entries():
+                if title in entry:
+                    entries.append(entry)
+                elif title.lower() in entry:
+                    entries.append(entry)
+                elif title.upper() in entry:
+                    entries.append(entry)
+                elif title.capitalize() in entry:
+                    entries.append(entry)
+            if entries:
+                    return render(request, "encyclopedia/result.html", {
+                        "entries": entries , "title": title
+                            })
+            else:
+                return render(request, "encyclopedia/error.html", {
+                    "title": title , "message": "The requested page was not found."
+                        })
+    else:
+        title = request.GET.get('q')
+        return render(request, "encyclopedia/error.html", {
+                "title": title , "message": "The requested page was not found."
+            })
